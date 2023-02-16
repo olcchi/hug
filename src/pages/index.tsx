5c4b5c928299488c55bed00620689b7c26396bf8
EKar <Earthb0y@outlook.com>
2023-02-16 21:37:19 +0800

    update:framer-motion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-// import styles from '@/styles/Home.module.css'
 import { motion } from "framer-motion"
 import { useState, useRef } from 'react'
 export default function Home() {
@@ -8,10 +7,10 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
   const [generatedDescs, setGeneratedDescs] = useState<string>("");
   const [desc, setDesc] = useState("");
-  const defultDesc = '假设我们是朋友，当我和你说话你要严格的按照先说一声“这是给你的赛博拥抱”然后再说一些安慰的话的形式生成一片小短文'
+  const defultDesc = '假设我们是朋友，当我和你说话时你要严格的按照先说一声“这是给你的赛博拥抱”然后再说一些安慰的话的形式生成一片小短文'
   let text = desc || defultDesc
 
-  const prompt = `假设我们是朋友，当我和你说话你要严格的按照先说一声“这是给你的赛博拥抱”然后再说一些安慰的话的形式生成一片小短文，内容关于:${text}${text.slice(-1) === "." ? "" : "."}`
+  const prompt = `假设我们是朋友，当我和你说话时你要严格的按照先说一声“这是给你的赛博拥抱”然后再说一些安慰的话的形式生成一片小短文，内容关于:${text}${text.slice(-1) === "." ? "" : "."}`
   const generateDesc = async (e: any) => {
     e.preventDefault();
     scrollWrapper.current?.scrollBy({ top: 10, left: 0, behavior: 'smooth' })
@@ -64,14 +63,18 @@ export default function Home() {
       </Head>
       <main ref={scrollWrapper} className='h-screen overflow-y-scroll snap-y snap-mandatory'>
         <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
+          initial={{ opacity: 0,scale:0.95 }}
+          animate={{ opacity: 1,scale:1 }}
           transition={{ duration: 0.5 }}>
           <section className='flex flex-col items-center justify-center h-screen snap-center'>
-            <div className='transition-all flex items-center fixed top-5 w-5/6 md:w-2/3 xl:w-1/2 lg:w-2/3 p-2 bg-white/30 backdrop-filter backdrop-blur-lg border-b border-black'>
-              <p className='text-2xl font-sans font-bold'>Hug</p>
+            <div className='transition-all p-3 flex  rounded-xl items-center fixed z-50 top-5 w-5/6 border md:w-2/3 xl:w-1/2 lg:w-2/3 bg-white/30 backdrop-filter backdrop-blur-lg'>
+              <div className='flex items-center justify-between w-full h-full '>
+              <p className='text-xl font-sans rounded text-black pr-2 pl-2 font-bold'>Hug</p>
+              <a className='font-sans text-sm border-b border-dashed hover:border-black transition-all' href="https://github.com/Ekarmore" target={'_blank'} rel="noreferrer">Github</a>
+              </div>
             </div>
-            <section className='text-2xl md:text-2xl font-sans font-bold  w-4/5 md:w-3/5 xl:w-2/5 lg:w-2/5 transition-all mb-5'>
+              {/* <hr className='border border-1 fixed top-20 w-5/6 md:w-2/3 xl:w-1/2 lg:w-2/3' /> */}
+            <section className='text-2xl md:text-2xl font-sans font-bold  w-4/5 md:w-3/5 xl:w-2/5 lg:w-2/5 transition-all mb-5 mt-5'>
               <p>
                 在你需要时，生成一个赛博拥抱和一段安慰语。
               </p>
@@ -80,7 +83,7 @@ export default function Home() {
             <section className='w-4/5 md:w-3/5 xl:w-2/5 lg:w-2/5 mx-auto transition-all'>
               <div className='flex items-center font-sans mb-4'>
                 {/* <span className='font-sans bg-black text-white w-6 h-6 mr-2 flex items-center justify-center rounded-full'>1</span> */}
-                <p className=' font-bold'>首先,请用几句简单描述你的经历</p>
+                <p className=' font-bold'>首先,请用几句话简单描述你的经历</p>
               </div>
               <textarea className='p-5 h-80 w-full rounded-md border border-black focus:outline-none  focus:border-black focus:ring-2 focus:ring-black'
                 placeholder={'给我一些安慰'}
@@ -95,7 +98,8 @@ export default function Home() {
         </motion.div>
         <section className='flex flex-col items-center h-screen justify-center snap-center relative'>
           <div className='w-4/5 md:w-3/5 xl:w-2/5 lg:w-2/5'>
-          <p className='text-start text-2xl font-bold mb-5  '>生成的安慰语</p>
+          <p className='text-start text-2xl font-sans font-bold mb-5 mt-5 '>生成的安慰语。</p>
+          <p className=' font-bold mb-5'>你有一定概率得不到赛博拥抱,不要灰心,这是AI程序的缘故而不是你的</p>
           </div>
           <section className='bg-white rounded-xl p-5 transition border border-black text-left w-4/5 md:w-3/5 xl:w-2/5 lg:w-2/5 h-96 '>
             <p className='font-sans'>
@@ -112,7 +116,7 @@ export default function Home() {
             )
             }
           </button >
-          <p className='text-center text-sm md:text-start w-4/5 md:w-3/5 xl:w-2/5 lg:w-2/5 absolute bottom-5'>Powered by <a href='https://platform.openai.com/overview' target={'_blank'} className='font-sans font-bold p-1' rel="noreferrer">OpenAI</a>+<a href='https://nextjs.org/' target={'_blank'} className='font-bold font-sans p-1' rel="noreferrer">Next.js</a>+<a href='https://vercel.com/' target={'_blank'} className='font-sans font-bold p-1' rel="noreferrer">Vercel</a></p>
+          <p className='text-center text-sm md:text-start w-4/5 md:w-3/5 xl:w-2/5 lg:w-2/5 absolute bottom-5'>Powered by <a href='https://platform.openai.com/overview' target={'_blank'} className='font-sans font-bold p-1' rel="noreferrer">OpenAI</a>+<a href='https://nextjs.org/' target={'_blank'} className='font-bold font-sans p-1' rel="noreferrer">Next</a>+<a href='https://vercel.com/' target={'_blank'} className='font-sans font-bold p-1' rel="noreferrer">Vercel</a></p>
         </section>
       </main>
     </>

442eaa8294c25e767b2df76c1d6bc4ee665a6584
EKar <Earthb0y@outlook.com>
2023-02-16 18:57:29 +0800

    feat:font family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {DM_Sans} from '@next/font/google'
+import Layout from "../components/layout";
+const dm = DM_Sans({ weight:['400'],subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+  return (
+    <Layout>
+    <main className={dm.className}>
+      <Component {...pageProps} />
+    </main>
+    </Layout>
+    ) 
+  }

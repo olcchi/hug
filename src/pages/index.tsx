fc7be2236eeacc09579cdab5a8ee77bdff62f3bc
EKar <Earthb0y@outlook.com>
2023-03-18 18:49:17 +0800

    feat:Update User Interface

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { motion } from "framer-motion"
+import Hug from '../components/Hug'
 import { useState, useRef } from 'react'
 export default function Home() {
   const scrollWrapper = useRef<HTMLDivElement>(null)
@@ -52,6 +53,9 @@ export default function Home() {
   const prevPage = () => {
     scrollWrapper.current?.scrollBy({ top: -10, left: 0, behavior: 'smooth' })
   }
+  const clearInput = () => {
+    setDesc('')
+  }
   return (
     <>
       <Head>
@@ -60,52 +64,43 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main ref={scrollWrapper} className='h-screen overflow-y-scroll snap-y snap-mandatory'>
+      <p className='font-sans font-bold text-2xl italic fixed top-0 left-0 p-5 text-white hover:scale-110 transition-all ease-in-out'>Hug</p>
+      <main ref={scrollWrapper} className='bg-neutral-900'>
         <motion.div
-          initial={{ opacity: 0,scale:0.9 }}
-          animate={{ opacity: 1,scale:1 }}
+          initial={{ opacity: 0, scale: 0.9 }}
+          animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}>
-          <section className='flex flex-col items-center justify-center h-screen snap-center'>
-            <div className='transition-all p-3 flex  rounded-xl items-center fixed z-50 top-5 w-5/6 border md:w-2/3 xl:w-1/2 lg:w-2/3 bg-white/30 backdrop-filter backdrop-blur-lg'>
-              <div className='flex items-center justify-between w-full h-full '>
-              <p className='text-xl font-sans rounded text-black pr-2 pl-2 font-bold'>Hug</p>
-              <a className='font-sans text-sm border-b border-dashed hover:border-black transition-all' href="https://github.com/Ekarmore" target={'_blank'} rel="noreferrer">Github</a>
-              </div>
-            </div>
-              {/* <hr className='border border-1 fixed top-20 w-5/6 md:w-2/3 xl:w-1/2 lg:w-2/3' /> */}
-            <section className='text-2xl md:text-2xl font-sans font-bold  w-4/5 md:w-3/5 xl:w-2/5 lg:w-2/5 transition-all mb-5 mt-5'>
-              <p>
-                在你需要时，生成一个赛博拥抱和一段安慰语。
-              </p>
+          <section className='flex flex-col items-center h-screen '>
+            <section className='flex flex-col items-center justify-center mt-40 mb-5 w-4/5 md:1/2 lg:w-3/5 xl:w-1/3 transition-all'>
+              <section className='text-xl xl:text-xl font-sans text-left w-full font-bold transition-all mt-5'>
+                <p className='text-white'>
+                  在你需要时，生成一个赛博拥抱和一段安慰语。
+                </p>
+              </section>
+              <section className='w-full mt-3 flex transition-all'>
+                <input
+                  className='flex-grow p-4 h-12 rounded text-white bg-neutral-800 hover:opacity-80 focus:outline-none  focus:border-neutral-600 focus:ring-2 focus:ring-neutral-600 transition-all'
+                  type="text"
+                  placeholder={'请给我一些安慰...'}
+                  value={desc}
+                  onChange={(e) => setDesc(e.target.value)}
+                />
+                <button className='flex-none w-24 h-12 mr-2 ml-2 rounded text-white hover:opacity-80 bg-neutral-800  font-bold transition-all'
+                  onClick={(e) => { generateDesc(e) }}>生成
+                </button>
+                <button className='flex-none w-24 h-12 rounded text-white hover:opacity-80 bg-neutral-800  font-bold transition-all'
+                  onClick={clearInput}>清空
+                </button>
+              </section>
             </section>
-            {/* <hr className='w-4/5 md:w-3/5 xl:w-2/5 lg:w-2/5 transition-all mt-5 mb-5 border-black' /> */}
-            <section className='w-4/5 md:w-3/5 xl:w-2/5 lg:w-2/5 mx-auto transition-all'>
-              <div className='flex items-center font-sans mb-4'>
-                {/* <span className='font-sans bg-black text-white w-6 h-6 mr-2 flex items-center justify-center rounded-full'>1</span> */}
-                <p className=' font-bold'>首先,请用几句话简单描述你的情况</p>
-              </div>
-              <textarea className='p-5 h-72 w-full rounded border border-black focus:outline-none  focus:border-black focus:ring-2 focus:ring-black'
-                placeholder={'请给我一些安慰...'}
-                value={desc}
-                onChange={(e) => setDesc(e.target.value)}
-              />
-            </section>
-            <button className='w-4/5 md:w-3/5 xl:w-2/5 lg:w-2/5 border h-10 rounded-xl text-white hover:opacity-80 bg-black  font-bold transition-all mt-5'
-              onClick={(e) => { generateDesc(e) }}>生成 &rarr;
-            </button>
-          </section>
-        </motion.div>
-        <section className='flex flex-col items-center h-screen justify-center snap-center relative'>
-          <div className='w-4/5 md:w-3/5 xl:w-2/5 lg:w-2/5'>
-          <p className='text-start text-2xl font-sans font-bold mb-5 mt-5 '>生成的安慰语。</p>
-          <p className=' font-bold mb-5'>你有一定概率得不到赛博拥抱,这是AI程序的缘故而不是你的</p>
-          </div>
-          <section className='bg-white rounded p-5 border border-black text-left w-4/5 md:w-3/5 xl:w-2/5 lg:w-2/5 h-72 overflow-y-scroll transition-all '>
-            <p className='font-sans'>
-              {generatedDescs}
-            </p>
-          </section>
-          <button onClick={prevPage} className='w-4/5 md:w-3/5 xl:w-2/5 lg:w-2/5 border h-10 font-bold rounded-xl text-white hover:opacity-80 bg-black transition-all mt-5 '>
+            <section
+             className='flex flex-col items-center w-4/5 md:1/2 lg:w-3/5 xl:w-1/3 transition-all'>
+              <section className='w-full h-48 bg-neutral-800 rounded p-5 text-left transition-all '>
+                <p className='font-sans'>
+                  {generatedDescs}
+                </p>
+              </section>
+              {/* <button onClick={prevPage} className='border h-10 font-bold rounded-xl text-white hover:opacity-80 bg-black transition-all mt-5 '>
             {loading && (
               <p>生成中...</p>
             )
@@ -114,9 +109,10 @@ export default function Home() {
               <p>我需要更多安慰</p>
             )
             }
-          </button >
-          <p className='text-center text-sm md:text-start w-4/5 md:w-3/5 xl:w-2/5 lg:w-2/5 absolute bottom-5'>Powered by <a href='https://platform.openai.com/overview' target={'_blank'} className='font-sans font-bold p-1' rel="noreferrer">OpenAI</a>+<a href='https://nextjs.org/' target={'_blank'} className='font-bold font-sans p-1' rel="noreferrer">Next</a>+<a href='https://vercel.com/' target={'_blank'} className='font-sans font-bold p-1' rel="noreferrer">Vercel</a></p>
-        </section>
+          </button > */}
+            </section>
+          </section>
+        </motion.div>
       </main>
     </>
   )

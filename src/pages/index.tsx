944f4ac56be958ff692ee76ed160f35c9a4afe18
EKar <Earthb0y@outlook.com>
2023-03-19 17:29:49 +0800

    feat:DarkMode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -75,9 +75,9 @@ export default function Home() {
         animate={{ opacity: 1}}
         transition={{duration: 0.5 }}
       >
-      <p className='font-sans font-bold text-2xl italic fixed top-0 left-0 p-5 text-white hover:scale-110 transition-all ease-in-out'>Hug</p>
+      <p className='font-sans font-bold text-2xl italic fixed top-0 left-0 p-5 text-black dark:text-white hover:scale-110 transition-all ease-in-out'>Hug</p>
       </motion.div>
-      <main className='bg-neutral-900 w-screen h-screen'>
+      <main className='bg-white dark:bg-neutral-900 w-screen h-screen'>
         <motion.div
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
@@ -85,32 +85,32 @@ export default function Home() {
           <section className='flex flex-col items-center '>
             <section className='flex flex-col items-center justify-center mt-40 mb-5 w-4/5 md:1/2 lg:w-3/5 xl:w-1/3 transition-all'>
               <section className='text-xl xl:text-xl font-sans text-left w-full font-bold transition-all mt-5'>
-                <p className='text-white'>
+                <p className='text-black dark:text-white'>
                   在你需要时，生成一个赛博拥抱和一段安慰语。
                 </p>
               </section>
               {!loading ? (<section className='w-full mt-3 flex transition-all'>
                 <input
-                  className='flex-grow flex-shrink p-4 h-12 rounded text-white bg-neutral-800 hover:opacity-80 focus:outline-none  focus:border-neutral-600 focus:ring-2 focus:ring-neutral-600 transition-all'
+                  className='flex-grow flex-shrink p-4 h-12 rounded text-black dark:text-white bg-gray-100 dark:bg-neutral-800 hover:opacity-80 focus:outline-none  focus:border-neutral-600 focus:ring-2 focus:ring-neutral-600 transition-all'
                   type="text"
                   placeholder={'请给我一些安慰...'}
                   value={desc}
                   onChange={(e) => setDesc(e.target.value)}
                 />
-                <button className='flex-shrink w-20 text-xs md:text-base md:w-24 h-12 mr-2 ml-2 rounded text-white hover:opacity-80 bg-neutral-800  font-bold transition-all'
+                <button className='flex-shrink w-20 text-xs md:text-base md:w-24 h-12 mr-2 ml-2 rounded text-black dark:text-white hover:opacity-80 bg-gray-100 dark:bg-neutral-800  font-bold transition-all'
                   onClick={(e) => { generateDesc(e) }}>生成
                 </button>
                 <button 
                   disabled = {desc?false:true}
-                  className='disabled:opacity-30 flex-shrink text-xs md:text-base w-20 md:w-24 h-12 rounded text-white hover:opacity-80 bg-neutral-800  font-bold transition-all'
+                  className='disabled:opacity-30 flex-shrink text-xs md:text-base w-20 md:w-24 h-12 rounded text-black dark:text-white hover:opacity-80 bg-gray-100 dark:bg-neutral-800  font-bold transition-all'
                   onClick={clearInput}>清空
                 </button>
               </section>):(<section className='w-full'>
                   <div
-                  className='flex items-center justify-center relative mt-3 w-full text-xs md:text-base h-12 rounded text-white  bg-neutral-800  font-bold transition-all animate-pulse'
+                  className='flex items-center justify-center relative mt-3 w-full text-xs md:text-base h-12 rounded text-black dark:text-white  dark:bg-neutral-800  bg-gray-100 font-bold transition-all animate-pulse'
                   >
                     生成中...
-                     <button className='bg-neutral-700 h-8 w-20 rounded text-xs absolute right-2 top-2'
+                     <button className='bg-gray-200 dark:bg-neutral-700 h-8 w-20 rounded text-xs absolute right-2 top-2'
                      onClick={stopGenerate}>
                       停止生成
                     </button>
@@ -125,8 +125,8 @@ export default function Home() {
             </section>
             {genBoxState?(<section
              className='flex flex-col items-center w-4/5 md:1/2 lg:w-3/5 xl:w-1/3 transition-all'>
-              <section className='w-full h-auto bg-neutral-800 rounded p-5 text-left transition-all '>
-                <p className='font-sans text-white'>
+              <section className='w-full h-auto bg-gray-100 dark:bg-neutral-800 rounded p-5 text-left transition-all '>
+                <p className='font-sans text-black dark:text-white'>
                   {generatedDescs}
                 </p>
               </section>
@@ -149,7 +149,7 @@ export default function Home() {
           animate={{ opacity: 1}}
           transition={{ duration: 0.5 }}
         >
-          <p className='text-xs opacity-70 fixed p-5 text-white bottom-0'>made by <a rel='noopener noreferrer' target={'_blank'} href="https://ekar.site/">ekar</a><span className='mr-2 ml-2'>|</span>
+          <p className='text-xs opacity-70 fixed p-5 text-black dark:text-white bottom-0'>made by <a rel='noopener noreferrer' target={'_blank'} href="https://ekar.site/">ekar</a><span className='mr-2 ml-2'>|</span>
           <a rel='noopener noreferrer' className='border-b border-dashed hover:opacity-70 transition-all text-xs' href='https://github.com/Ekarmore/hug' target={'_blank'}>Github</a></p>
         </motion.div>
       </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode:'media',
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
